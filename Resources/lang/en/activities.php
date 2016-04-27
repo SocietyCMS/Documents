4bf0bb1b8c7a9942af40a31791d9a76548e21d47
Ralph Huwiler <ralph@huwiler.rocks>
2016-04-27 22:20:30 +0200

    localizing activities

---
+++ Resources/lang/en/activities.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'created_object' => [
+        'summary' => '{0} :user uploaded <a href=":url">:file</a> to :pool.|{1} :user uploaded <a href=":url">:file</a> and one more file to :pool.|]1,Inf[ :user uploaded <a href=":url">:file</a> and :count more files to :pool.',
+    ],
+];
--- Resources/views/backend/activities/created_object.blade.php
+++ Resources/views/backend/activities/created_object.blade.php
@@ -6,7 +6,16 @@
             </div>
             <div class="content">
                 <div class="summary">
-                    {{$activity->user->present()->fullname}} uploaded <a href="{{route('backend::documents.documents.index')}}#!/{{$activity->subject->pool->uid}}/{{$activity->subject->parent_uid?:'null'}}">{{$activity->subject->title}}</a> to '{{$activity->subject->pool->title}}'
+                    {!!  trans_choice(
+                    'documents::activities.created_object.summary',
+                    $activity->count_in_group -1,
+                    [
+                        'user' => $activity->user->present()->fullname,
+                        'file' => $activity->subject->title,
+                        'url' => route('backend::documents.documents.index')."#!/{$activity->subject->pool->uid}/".$activity->subject->parent_uid?:'null',
+                        'pool' => $activity->subject->pool->title
+                    ]
+                    ) !!}
                     <div class="date">
                         {{$activity->created_at->diffForHumans()}}
                     </div>
